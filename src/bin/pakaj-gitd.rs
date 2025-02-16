f39b177f6c4c384d0c2bfb30682f7a9eb680cd12
redstone <31410740+redstone-dev@users.noreply.github.com>
2025-02-16 22:39:53 +0000

    thanks Atthun for fixing stuff

--- src/bin/pakaj-gitd.rs
+++ src/bin/pakaj-gitd.rs
@@ -11,16 +11,16 @@ fn update_all_packages() -> Result<()> {
     read_dir("packages")
         .unwrap()
         .for_each(|package_directory_path| {
-            let path = package_directory_path.unwrap().path();
+            let path = &package_directory_path.unwrap().path();
             
             // pull from repo if package.git.remote is true, to update package
             // also trigger build script
-            let pkg_meta_toml = toml::from_str::<toml::Table>(&read_to_string(path).unwrap()).unwrap();
+            let pkg_meta_toml = toml::from_str::<toml::Table>(&read_to_string(&path).unwrap()).unwrap();
             if pkg_meta_toml["package"]["git"]["remote"].as_bool().unwrap() {
                 Command::new("git")
                     .args(["pull"])
                     .output()
-                    .unwrap_or_else(|_| panic!("{:?}: git pull failed :(", &path));
+                    .unwrap_or_else(|_| panic!("{:?}: git pull failed :(", path));
             }
         });
     Ok(())
