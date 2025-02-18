9a19a828508b3371f1d510d7dae7ce89e02b4dc1
redstone <31410740+redstone-dev@users.noreply.github.com>
2025-02-18 00:04:05 +0000

    beginning of cli

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,4 +1,15 @@
+use clap::Parser;
 
+enum CliSubcommand {
+    Add,
+    Update,
+    Remove
+}
+
+struct Cli {
+    command: CliSubcommand,
+    package_path: std::path::PathBuf,
+}
 
 fn main() {
     println!("Hello {}", "world");
